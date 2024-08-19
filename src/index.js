435c1c1fe1f6d52b1e7436e74a34d72893c63c04
Santiago Nahuel Ruiz Sugliani <sruizs@fi.uba.ar>
2024-08-19 14:10:49 -0300

    Prueba commit con fix en index.js

--- src/index.js
+++ src/index.js
@@ -5,21 +5,28 @@ import App from './App';
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"; // Para Firestore
+import { getAuth } from "firebase/auth"; // Para Authentication
+import { getAnalytics } from "firebase/analytics"; // Para Analytics (opcional)
 
-// Your web app's Firebase configuration
+// Tu configuración de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyB7vPlLKwSYPR8K-7DKRUf_3SoAX9-6O5U",
-  authDomain: "fifa-store-51bda.firebaseapp.com",
-  projectId: "fifa-store-51bda",
-  storageBucket: "fifa-store-51bda.appspot.com",
-  messagingSenderId: "950670174365",
-  appId: "1:950670174365:web:ca225e42aca7c3f8b88c74"
+  apiKey: "AIzaSyDK0JsrXUewve3l8xa2rdTzDrERv6zJGUs",
+  authDomain: "fifa-store.firebaseapp.com",
+  projectId: "fifa-store",
+  storageBucket: "fifa-store.appspot.com",
+  messagingSenderId: "1005134943864",
+  appId: "1:1005134943864:web:4532d0e0780fe2011fff8d",
+  measurementId: "G-XXXXXXX" // Solo si usas Analytics
 };
 
 // Initialize Firebase
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+// Initialize other Firebase services
+const db = getFirestore(app); // Inicializa Firestore
+const auth = getAuth(app); // Inicializa Authentication
+const analytics = getAnalytics(app); // Inicializa Analytics (opcional)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
