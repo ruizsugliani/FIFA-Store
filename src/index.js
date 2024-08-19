24e30a1c47ebb212bcff8075091e910cb7af7ad1
Santiago Nahuel Ruiz Sugliani <sruizs@fi.uba.ar>
2024-08-19 14:30:19 -0300

    Cambios en uso de credenciales

--- src/index.js
+++ src/index.js
@@ -11,14 +11,14 @@ import { getAnalytics } from "firebase/analytics"; // Para Analytics (opcional)
 
 // Tu configuración de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDK0JsrXUewve3l8xa2rdTzDrERv6zJGUs",
-  authDomain: "fifa-store.firebaseapp.com",
-  projectId: "fifa-store",
-  storageBucket: "fifa-store.appspot.com",
-  messagingSenderId: "1005134943864",
-  appId: "1:1005134943864:web:4532d0e0780fe2011fff8d",
-  measurementId: "G-XXXXXXX" // Solo si usas Analytics
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
